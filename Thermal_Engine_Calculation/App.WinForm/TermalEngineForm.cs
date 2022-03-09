6ed79465b4ea30eb381988d9520f1545423a0643
PavlovNick <pavlovmikola01@gmail.com>
2022-03-09 17:32:02 +0200

    Створили макет

--- Thermal_Engine_Calculation/App.WinForm/TermalEngineForm.cs
+++ Thermal_Engine_Calculation/App.WinForm/TermalEngineForm.cs
@@ -10,47 +10,9 @@ namespace Thermal_Engine_Calculation.App.WinForm
 {
     public partial class TermalEngineForm : Form
     {
-        Color BackColorMain = Color.FromArgb(0, 102, 204);
-        Color BackColorTool = Color.FromArgb(0, 102, 204);
-        Color BackColorResetButton = Color.FromArgb(0, 102, 204);
-        Color BackColorCalculateButton = Color.FromArgb(0, 102, 204);
-        Color ForeColorMain = Color.FromArgb(0, 102, 204);
-
-
         public TermalEngineForm()
         {
             InitializeComponent();
-
-            //#region TermalEngineForm
-            //ClientSize = new System.Drawing.Size(800, 600);
-            //MinimumSize = new System.Drawing.Size(100, 100);
-            //#endregion
-
-            //#region panelForButton
-            //this.panelForButton.Size = new System.Drawing.Size(350, 450);
-            //#endregion
-
-            //#region panelForEnter
-            //this.panelForEnter.Size = new System.Drawing.Size(550, 140);
-            //this.panelForEnter.MinimumSize = new System.Drawing.Size(500, 120);
-            //this.panelForEnter.BackColor = BackColorMain;
-            //#endregion
-
-            //#region panelForResetAndCalculate
-            //this.panelForResetAndCalculate.Size = new System.Drawing.Size(550, 86);
-            //#endregion
-
-            //#region panelForReset
-            //this.panelForReset.Size = new System.Drawing.Size(270, 86);
-            //#endregion
-
-            //#region panelForCalculate
-            //this.panelForCalculate.Size = new System.Drawing.Size(270, 86);
-            //#endregion
-
-            //#region panelForListing
-            //this.panelForListing.Size = new System.Drawing.Size(550, 328);
-            //#endregion
 
             #region Add button
             for (int i = 0; i < 30; i++)
@@ -63,16 +25,6 @@ namespace Thermal_Engine_Calculation.App.WinForm
                 });
             }
             #endregion
-        }
-
-        private void TermalEngineForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panelForListing_Paint(object sender, PaintEventArgs e)
-        {
-
         }
     }
 }
